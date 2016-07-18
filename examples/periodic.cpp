550b26c054b12785e0c4129ec003d9accae86dca
Gavin Lobo <gavin.n.lobo@gmail.com>
2016-07-18 17:12:58 -0400

    Added the Periodic class

---
+++ examples/periodic.cpp
@@ -0,0 +1,68 @@
+#include <iostream>
+#include <gnl/gnl_periodic.h>
+
+using namespace std;
+using namespace gnl;
+
+int process(int x )
+{
+    static int i=0;
+    int j = i++;
+    std::cout << "Process started: " << j << std::endl;
+    std::this_thread::sleep_for( std::chrono::seconds( rand() % 2 + 3) );
+    std::cout << "Process ended: " << j  << std::endl;
+    return 0;
+}
+
+struct Add
+{
+    void inc(int y) {
+        x+=y;
+    }
+
+    int x = 0;
+};
+
+int main( )
+{
+    if(0)
+    {
+        {
+            std::cout << "Testing Spawner mode:" << std::endl;
+            std::cout << "  " << std::endl;
+
+            std::cout << " Spawning a new thread every second for 50 spawns" << std::endl;
+            auto T = Periodic::Start_Spawner_Delayed<int>( std::bind( &process , 3 ) , 1000000, 50 );
+
+            std::cout << " Sleep for 10 seconds... Only 10 threads will be spawned" << std::endl;
+            std::this_thread::sleep_for( std::chrono::seconds(10) );
+
+            std::cout << "Exiting scope. Attempting to Destruct the Periodic." << std::endl;
+            std::cout << "No more threads will be spawned. Wait till current threads finish" << std::endl;
+        }
+        std::cout << "** Scope exited. All threads finished ** " << std::endl;
+
+    }
+
+    {
+        Add add;
+
+        std::cout << "Calling the add::inc function once every second with the parameter argument = 1." << std::endl;
+
+        auto T2 = Periodic::Start<void>( std::bind( &Add::inc, &add , 1 ) , 1000000, 10 );
+
+        std::cout << "Sleeping for 5 seconds, then going to stop" << std::endl;
+
+        std::this_thread::sleep_for( std::chrono::seconds(5) );
+
+        std::cout << " Calling Stop() on Periodic" << std::endl;
+        T2->Stop();
+
+        std::cout << "Value of Add::x = " << add.x << std::endl;
+
+    }
+
+
+    //std::cout << "Currently running threads: " << T->Running_Threads() << std::endl;
+    return 0;
+}
---
+++ gnl/gnl_periodic.h
@@ -0,0 +1,325 @@
+#ifndef GNL_PERIODIC_H
+#define GNL_PERIODIC_H
+
+#include <chrono>
+#include <functional>
+#include <atomic>
+#include <iostream>
+#include <cstdint>
+#include <limits>
+#include <vector>
+#include <future>
+#include <algorithm>
+
+
+#ifndef _MSC_VER
+#define NOEXCEPT noexcept
+#else
+#define NOEXCEPT
+#endif
+
+
+namespace gnl
+{
+/**
+ * @brief The Periodic class
+ *
+ * A class that will periodically call a function at a given interval.
+ *
+ * This class can operate in two modes, Regular or Spawner.
+ *
+ * In regular mode, the function is called at regular intervals.
+ *
+ * In spawner mode, the function will be spawned in a new thread at
+ * regular intervals. This is useful if the function that is to be
+ * called might take a bit longer than you'd like it and would prefer
+ * to run it in the background.
+ *
+ * Destroying the Periodic class will wait for all threads to finish.
+ *
+ * A periodic must be constructed on the heap if you want to be able to copy
+ * or move the object. This is why static factor methods are provided
+ * to construct shared pointers to a Periodic class.
+ *
+ */
+class Periodic
+{
+public:
+    ~Periodic()
+    {
+        if( !__quit_flag.load( std::memory_order_acquire ) )
+        {
+            Stop();
+        };
+    }
+
+    /**
+     * @brief Stop
+     *
+     * Stops the execution of the function call.
+     */
+    void Stop()
+    {
+        // __is_running.store( false, std::memory_order_release );
+        if( __isrunning )
+        {
+            __quit_flag.store(  true, std::memory_order_release );
+            Wait();
+        }
+    }
+
+
+    /**
+     * @brief Wait
+     *
+     * Waits for the function call to finish. This will wait forever if you are calling the function an infinite amount of times!
+     */
+    void Wait()
+    {
+        if( __runfuture.valid() )
+        {
+            //std::cout << "Waiting " << std::endl;
+            __runfuture.get();
+        }
+        //if( __runthread.joinable() )
+        //{
+        //   // std::cout << "Waiting for __runthread to finish" << std::endl;
+        //    __runthread.join();
+        //}
+    }
+
+    /**
+     * @brief Is_Running
+     * @return
+     *
+     * Returns true if the timer is running and is calling functions.
+     */
+    bool Is_Running() const NOEXCEPT
+    {
+        //auto joinable  = __runthread.joinable();
+        //auto isrunning =__is_running.load(std::memory_order_acquire);
+        return __isrunning;
+        //auto future_ready = (__runfuture.wait_for(std::chrono::milliseconds(0)) != std::future_status::ready);
+        //
+        //return future_ready;
+        //auto isrunning    = __is_running.load(std::memory_order_acquire);
+
+        // std::cout << "isready: "  << future_ready << std::endl;
+        // std::cout << "isrunning:" << isrunning << std::endl;
+
+        //  return (  isrunning && future_ready);
+    }
+
+
+    int Running_Threads() const
+    {
+        return __runningthreads;
+    }
+    /**
+     * @brief Start_Delayed
+     * @param func - the function to call, use std::bind to bind a function to a function objects
+     * @param microseconds - the number of microseconds to wait between calls
+     * @param count - number of times to call teh function
+     * @return A shared pointer to a Periodic class
+     *
+     * Static factory function that starts calling the function, func, but delays the execution of the first call using the definied interval.
+     *
+     */
+    template<typename Rettype>
+    static std::shared_ptr< Periodic > Start_Delayed( std::function<Rettype(void)> func , std::uint64_t microseconds , std::uint64_t count = std::numeric_limits<std::uint64_t>::max() )
+    {
+        std::shared_ptr< Periodic > P( new Periodic() );
+
+        P->__interval = std::chrono::microseconds(microseconds);
+        P->Execute< Rettype >( func , std::chrono::microseconds( microseconds ) , count, true);
+        return P;
+    }
+
+
+    /**
+     * @brief Start
+     * @param func - the function to call, use std::bind to bind a function to a function objects
+     * @param microseconds - the number of microseconds to wait between calls
+     * @param count - number of times to call the function
+     * @return A shared pointer to a Periodic class
+     *
+     * Static factory function that starts calling the function immediately.
+     */
+    template<typename Rettype>
+    static std::shared_ptr< Periodic > Start( std::function<Rettype(void)> func , std::uint64_t microseconds , std::uint64_t count = std::numeric_limits<std::uint64_t>::max() )
+    {
+        std::shared_ptr< Periodic > P( new Periodic( std::chrono::microseconds(microseconds), false, count) );
+
+        P->Execute< Rettype >( func );
+        return P;
+    }
+
+
+    /**
+     * @brief Start_Spawner
+     * @param func - the function to call, use std::bind to bind a function to a function objects
+     * @param microseconds - the number of microseconds to wait between calls
+     * @param count - number of times to call teh function
+     *
+     * Static factory function that starts the Periodic in spawner mode, that is, everytime func() is called, it will be called in a new thread.
+     */
+    template<typename Rettype>
+    static std::shared_ptr< Periodic > Start_Spawner( std::function<Rettype(void)> func , std::uint64_t microseconds , std::uint64_t count = std::numeric_limits<std::uint64_t>::max() )
+    {
+        std::shared_ptr< Periodic > P( new Periodic( std::chrono::microseconds(microseconds), true, count) );
+
+        P->__interval = std::chrono::microseconds(microseconds);
+        P->Execute< Rettype >( func );
+        return P;
+    }
+
+    /**
+     * @brief Start_Spawner
+     * @param func - the function to call, use std::bind to bind a function to a function objects
+     * @param microseconds - the number of microseconds to wait between calls
+     * @param count - number of times to call teh function
+     * @return A shared pointer to a Periodic class
+     *
+     * Starts the Periodic in spawner mode, that is, everytime func() is called, it will be called in a new thread.
+     */
+    template<typename Rettype>
+    static std::shared_ptr< Periodic > Start_Spawner_Delayed( std::function<Rettype(void)> func , std::uint64_t microseconds , std::uint64_t count = std::numeric_limits<std::uint64_t>::max() )
+    {
+        std::shared_ptr< Periodic > P( new Periodic( std::chrono::microseconds(microseconds), true, count, true) );
+
+        P->__interval = std::chrono::microseconds(microseconds);
+
+        P->Execute< Rettype >( func  );
+        return P;
+    }
+
+
+
+
+    template<typename Rettype>
+    bool __run(std::function<Rettype(void)> func )
+    {
+        if( this->__start_delayed)
+            std::this_thread::sleep_for( this->__interval );
+
+        std::vector< std::future<bool> > thread_map;
+
+        //std::cout << "============RUN CALLED ====================" << std::endl;
+       // std::cout << "Count: " << this->__count << std::endl;
+
+
+        while( !__quit_flag.load(std::memory_order_acquire) && this->__count )
+        {
+          //  std::cout << "quit flag : " << this->__quit_flag.load(std::memory_order_acquire) << std::endl;
+
+            if( this->__spawner_mode )
+            {
+                auto future = std::async(std::launch::async, [this, func]
+                {
+                    this->__runningthreads++;
+                    func();
+                    return true;
+                });
+
+
+                thread_map.push_back( std::move(future) );
+
+                if( thread_map.size() > 10 )
+                {
+
+                    thread_map.erase(std::remove_if(thread_map.begin(),
+                                                    thread_map.end(),
+                                                    [this](std::future<bool> & x)
+                    {
+                                         auto fin = x.wait_for(std::chrono::milliseconds(0))==std::future_status::ready;
+                                         if( fin  ) this->__runningthreads--;
+                                         return fin;
+                                     }
+                                     ),
+                            thread_map.end()) ;
+
+                }
+
+                std::this_thread::sleep_for( this->__interval );
+            }
+            else
+            {
+                //std::cout << "Calling function: " << std::endl;
+                func();
+                // std::cout << "Sleeping: " << this->__interval.count()  << std::endl;
+                std::this_thread::sleep_for( this->__interval );
+            }
+            --this->__count;
+
+        }
+
+       // std::cout << "Loop exited.  Size: " << thread_map.size() << std::endl;
+        for(int i=0 ; i < thread_map.size() ; i++)
+        {
+            thread_map[i].get();
+            this->__runningthreads--;
+        }
+
+        thread_map.clear( );
+        this->__isrunning = false;
+        return true;
+    }
+
+
+private:
+    Periodic( std::chrono::microseconds interval = std::chrono::microseconds(1000000),
+              bool spawner_mode = false,
+              std::uint64_t count = std::numeric_limits<std::uint64_t>::max(),
+              bool delayed = false
+            )
+        : __quit_flag(false), __spawner_mode(spawner_mode), __count(count), __interval(interval), __start_delayed(delayed)
+    {}
+
+    template<typename Rettype>
+    /**
+         * @brief Execute
+         * @param func - the function object to call
+         * @param interval - the time interval to wait before periodic calls
+         * @param count - number of times to call the function
+         * @param start_delayed - true or false to start the timer in delayed mode
+         */
+    void Execute( std::function<Rettype(void)> func )
+    {
+        if( !__quit_flag.load( std::memory_order_acquire ) )
+        {
+            Stop();
+        };
+        //   Wait();
+        __quit_flag.store(false, std::memory_order_release);
+
+        //========================== Run in regular mode ==============================
+        //std::cout << "Running in regular mode: " << this->__quit_flag.load(std::memory_order_acquire) << std::endl;
+        this->__isrunning = true;
+        __runfuture = std::async( std::launch::async, std::bind( &Periodic::__run<Rettype>, this, func) );
+    }
+
+
+private:
+
+    bool                         __spawner_mode = false;
+    int                          __runningthreads = 0;
+    std::chrono::microseconds    __interval = std::chrono::microseconds(1000000);
+    std::future<bool>            __runfuture;
+    std::uint64_t                __count = 10;
+    bool                         __start_delayed = false;
+
+    bool                         __isrunning = false;
+
+    std::atomic<bool>            __quit_flag;
+
+
+};
+
+
+
+
+
+}
+
+#endif
+
