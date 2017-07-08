5ccfff8c8519f8d21f2c3fa9adefcecd598d3721
Gavin Lobo <gavin.n.lobo@gmail.com>
2017-07-08 10:58:37 -0400

    Added new linear_interp

--- tests/interp_test.cpp
+++ tests/interp_test.cpp
@@ -12,6 +12,7 @@ TEST_CASE( "Testing Parsing of Unquoted Keys" )
     std::vector<float> y = {0,1,2,3};
 
     gnl::linear_spline<float> L(t,y);
+    gnl::linear_spline2<float> L2(t,y);
 
     REQUIRE( fabs(L(0.0) -  0.0) < 0.0001 );
     REQUIRE( fabs(L(1.0) -  1.0) < 0.0001 );
@@ -20,6 +21,16 @@ TEST_CASE( "Testing Parsing of Unquoted Keys" )
 
     REQUIRE( fabs(L(1.5) -  1.5) < 0.0001 );
     REQUIRE( fabs(L(2.5) -  2.5) < 0.0001 );
-    REQUIRE( fabs(L(4)   -  2.5) < 0.0001 );
+
+    REQUIRE( fabs(L2(0.0) -  0.0) < 0.0001 );
+    REQUIRE( fabs(L2(1.0) -  1.0) < 0.0001 );
+    REQUIRE( fabs(L2(2.0) -  2.0) < 0.0001 );
+    REQUIRE( fabs(L2(3.0) -  3.0) < 0.0001 );
+
+    REQUIRE( fabs(L2(1.5) -  1.5) < 0.0001 );
+    REQUIRE( fabs(L2(2.5) -  2.5) < 0.0001 );
+
+    REQUIRE( fabs(L2.at(5.0) -  5.0) < 0.0001 );W
+
 
 }
