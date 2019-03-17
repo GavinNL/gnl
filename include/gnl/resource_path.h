e28ea4afc6c707f589d6054b5325a05a75c636bd
Gavin Lobo <gavin.n.lobo@gmail.com>
2019-03-17 15:54:11 -0400

    Added function to add paths using linux PATH style list

--- include/gnl/resource_path.h
+++ include/gnl/resource_path.h
@@ -54,6 +54,7 @@
 #include <exception>
 #include <stdexcept>
 #include <stdio.h>
+#include <sstream>
 
 #if defined __linux__
     #include <unistd.h>
@@ -118,6 +119,27 @@ class resource_path
             return true;
         }
 
+        /**
+         * @brief add_paths
+         * @param paths
+         * @param delimeter
+         * @return
+         *
+         *  Add paths using the standard linux way of listing paths:
+         *
+         *   paths="/home/user/path1:/home/path2:/usr/local/share/resources
+         */
+        void add_paths(path_type const & paths, char delimiter=':')
+        {
+           if(paths.size()==0) return;
+
+           std::string token;
+           std::istringstream tokenStream(paths);
+           while (std::getline(tokenStream, token, delimiter))
+           {
+              add_path(token);
+           }
+        }
         /**
          * @brief remove_path
          * @param path_
