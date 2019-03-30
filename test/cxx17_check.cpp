17927a76b34327673d7631dc781dfd1ce57be932
Gavin Lobo <gavin.n.lobo@gmail.com>
2019-03-30 10:35:55 -0400

    Removed message_bus example

--- test/cxx17_check.cpp
+++ test/cxx17_check.cpp
@@ -1,10 +1,12 @@
 #include <any>
 #include <variant>
+#include <optional>
 
 int main()
 {
     std::any x;
     std::variant<int, float> y = 1;
     std::get<int>(y);
+    std::optional<int> op;
     return 0;
 }
